180cddc65f1ddb0893d5932f09d0861e78b47927
Maurício <mauro.cavalcante18@gmail.com>
2024-06-25 18:31:22 -0300

    criando tarefas e lista de tarefas

---
+++ src/list/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,13 @@
+package list.OperacoesBasicas;
+
+public class Tarefa {
+    //atributo
+    private String descricao;
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+
+}
