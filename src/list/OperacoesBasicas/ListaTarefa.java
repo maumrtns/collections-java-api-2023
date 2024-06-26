46acd71397bc0b12125d178aec2ebb1e5e6c9f05
Maurício <mauro.cavalcante18@gmail.com>
2024-06-26 19:27:29 -0300

    Resolvendo ListaTarefa
    Signed-off-by: Maurício <mauro.cavalcante18@gmail.com>

--- src/list/OperacoesBasicas/ListaTarefa.java
+++ src/list/OperacoesBasicas/ListaTarefa.java
@@ -17,19 +17,58 @@ public class ListaTarefa {
 
     public void removerTarefa(String descricao) {
         List<Tarefa> tarefasParaRemover = new ArrayList<>();
-        for (Tarefa t : tarefaList) {
-            if (t.getDescricao().equalsIgnoreCase(descricao)) {
-                tarefasParaRemover.add(t);
+        if (!tarefaList.isEmpty()) {
+            for (Tarefa t : tarefaList) {
+                if (t.getDescricao().equalsIgnoreCase(descricao)) {
+                    tarefasParaRemover.add(t);
+                }
             }
+            tarefaList.removeAll(tarefasParaRemover);
+        } else {
+            System.out.println("A lista está vazia!");
         }
-        tarefaList.removeAll(tarefasParaRemover);
     }
 
-    public int obterNumeroTotalTarefas(){
+    public int obterNumeroTotalTarefas() {
         return tarefaList.size();
     }
 
-    public void obterDescricoesTarefas(){
-        System.out.println(tarefaList);
+    public void obterDescricoesTarefas() {
+        if (!tarefaList.isEmpty()) {
+            System.out.println(tarefaList);
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+    }
+
+    public static void main(String[] args) {
+        // Criando uma instância da classe ListaTarefa
+        ListaTarefa listaTarefa = new ListaTarefa();
+
+        // Adicionando tarefas à lista
+        listaTarefa.adicionarTarefa("Comprar leite");
+        listaTarefa.adicionarTarefa("Estudar para o exame");
+        listaTarefa.adicionarTarefa("Fazer exercícios");
+
+        // Exibindo o número total de tarefas na lista
+        System.out.println("Você tem " + listaTarefa.obterNumeroTotalTarefas() + " tarefas na lista:");
+
+        // Exibindo as descrições das tarefas na lista
+        listaTarefa.obterDescricoesTarefas();
+
+        // Removendo uma tarefa da lista
+        listaTarefa.removerTarefa("Trabalhar");
+
+        // Exibindo o número total de tarefas na lista após a remoção
+        System.out.println("Agora você tem " + listaTarefa.obterNumeroTotalTarefas() + " tarefas na lista:");
+
+        // Exibindo as descrições das tarefas atualizadas na lista
+        listaTarefa.obterDescricoesTarefas();
+
+        // Removendo uma tarefa da lista quando a lista está vazia
+        listaTarefa.removerTarefa("Estudar para o exame");
+
+        // Exibindo o número total de tarefas na lista após tentar remover de uma lista vazia
+        System.out.println("Agora você tem " + listaTarefa.obterNumeroTotalTarefas() + " tarefas na lista:");
     }
 }
