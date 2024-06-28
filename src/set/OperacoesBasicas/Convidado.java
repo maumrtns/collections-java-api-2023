a23911074f154f2534cbf15491a0faf15b141927
Maurício Martins <mauriciocamartins@gmail.com>
2024-06-28 17:46:43 -0300

    Operacoes basicas - Set
    Signed-off-by: Maurício Martins <mauriciocamartins@gmail.com>

---
+++ src/set/OperacoesBasicas/Convidado.java
@@ -0,0 +1,28 @@
+package set.OperacoesBasicas;
+
+public class Convidado {
+    //atributos
+    private String nome;
+    private int codigoConvite;
+
+    public Convidado(String nome, int codigoConvite) {
+        this.nome = nome;
+        this.codigoConvite = codigoConvite;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCodigoConvite() {
+        return codigoConvite;
+    }
+
+    public String toString() {
+        return "Convidado{" +
+                "nome='" + nome + '\'' +
+                ", codigoConvite=" + codigoConvite +
+                '}';
+    }
+}
+
